81af4fca913527665e77065f7d5518ea47c6faa3
¨pepe¨ <pepegarciagb@gmail.com>
2024-12-09 16:39:48 +0100

    6

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,18 +1,30 @@
-'use client';
-
 import Link from 'next/link';
 
 export default function Sidebar() {
   return (
-    <aside className="bg-gray-200 p-4 w-60 h-full">
-      <nav>
-        <ul className="space-y-4">
-          <li><Link href="/">Inicio</Link></li>
-          <li><Link href="/clients">Clientes</Link></li>
-          <li><Link href="/projects">Proyectos</Link></li>
-          <li><Link href="/deliverynotes">Albaranes</Link></li>
-        </ul>
-      </nav>
-    </aside>
+    <nav className="horizontal-navbar">
+      <ul className="nav-links">
+        <li>
+          <Link href="/" className="nav-link">
+            Inicio
+          </Link>
+        </li>
+        <li>
+          <Link href="/clients" className="nav-link">
+            Clientes
+          </Link>
+        </li>
+        <li>
+          <Link href="/projects" className="nav-link">
+            Proyectos
+          </Link>
+        </li>
+        <li>
+          <Link href="/deliverynotes" className="nav-link">
+            Albaranes
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
